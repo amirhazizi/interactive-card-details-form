c64fcddf1fc512110e5e3502a9ef19f81ee40f79
amir <amirazizihosein@gmail.com>
2023-02-04 10:51:52 +0330

    credit number checker js

--- js/app.js
+++ js/app.js
@@ -23,14 +23,17 @@ form.addEventListener("submit", function (e) {
     const id = input.dataset.id
     if (!value) {
       showError(input, id, "blank")
+    } else if (id === "card-number" && !creditNumber(value)) {
+      showError(input, id, "format")
+      console.log("number")
     } else if (id !== "card-name" && !onlyNumbers(value)) {
       showError(input, id, "format")
     } else {
       if (id === "card-number" && value.length < 16) {
         showError(input, id, "invalid")
-      } else if (id === "card-month" && value.length >= 3) {
+      } else if (id === "card-month" && value.length != 2) {
         showError(input, id, "invalid")
-      } else if (id === "card-year" && value.length >= 3) {
+      } else if (id === "card-year" && value.length != 2) {
         showError(input, id, "invalid")
       } else if (id === "card-cvc" && (value.length > 3 || value.length < 3)) {
         showError(input, id, "invalid")
@@ -50,40 +53,51 @@ form.addEventListener("submit", function (e) {
 function onlyNumbers(str) {
   return /^\d+$/.test(str)
 }
-
+function creditNumber(str) {
+  var myRegExp = /[0-9]{4}?[-\s]?[0-9]{4}[-\s]?[0-9]{4}[-\s]?[0-9]{4}$/im
+  return myRegExp.test(str)
+}
+// show error function
 function showError(input, id, action) {
   input.style.borderColor = "red"
   errorMsg.forEach(function (er) {
     if (er.dataset.id == id) {
-      er.classList.add("active")
-      if (action == "blank") {
-        er.textContent = `can't be blank`
-      } else if (action == "format") {
-        er.textContent = `wrong format numbers only`
-      } else {
-        er.textContent = `unvalid number`
-      }
-    } else if (id == "card-month" || id == "card-year") {
+      addClass(er, action)
+    } else if (id == "card-month") {
       if (er.dataset.id == "card-date") {
-        er.classList.add("active")
-        if (action == "blank") {
-          er.textContent = `can't be blank`
-        } else if (action == "format") {
-          er.textContent = `wrong format numbers only`
-        } else {
-          er.textContent = `unvalid number`
-        }
+        addClass(er, action)
+      }
+    } else if (id == "card-year") {
+      if (er.dataset.id == "card-date") {
+        addClass(er, action)
       }
     }
   })
 }
+// show error and change error text
+function addClass(er, action) {
+  er.classList.add("active")
+  if (action == "blank") {
+    er.textContent = `can't be blank`
+  } else if (action == "format") {
+    er.textContent = `wrong format numbers only`
+  } else {
+    er.textContent = `unvalid number`
+  }
+}
+// unshow Error function
 function unShowError(input, id) {
   input.style.borderColor = "hsl(279, 6%, 55%)"
   errorMsg.forEach(function (er) {
     if (er.dataset.id == id) {
       er.classList.remove("active")
       er.textContent = ``
-    } else if (id == "card-month" || id == "card-year") {
+    } else if (id == "card-month") {
+      if (er.dataset.id == "card-date") {
+        er.classList.remove("active")
+        er.textContent = ``
+      }
+    } else if (id == "card-year") {
       if (er.dataset.id == "card-date") {
         er.classList.remove("active")
         er.textContent = ``
